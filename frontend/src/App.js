7f35e15bfd15f1bbd89bc0a43d49707a093a674b
Papi Diagne <abdiagne96@gmail.com>
2022-07-18 23:28:15 +0000

    feat(frontend): design app header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app-container">
+      <div className="app-header">
+        <h1>ReleaseCheck</h1>
+        <h3>Your all-in one release checklist tool</h3>
+      </div>
+      <div className="app-content">
+        Content
+      </div>
     </div>
   );
 }
