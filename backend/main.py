5566bf2e779ae3f0836302b9ac36420d6fd7333c
Papi Diagne <abdiagne96@gmail.com>
2022-07-18 16:07:04 +0000

    feat(backend): implement get_releases and create_release endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,37 @@
 from fastapi import FastAPI
 from .schemas.Release import ReleaseSchema, ReleaseSchemaCreateIn, ReleaseSchemaUpdateIn
+from .db.database import database
+from .models.release_model import releases
+from typing import List
 
 app = FastAPI()
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
 
 @app.get("/")
 def home():
     return "Full Stack assignment server up and running."
 
-@app.get("/releases/")
-def get_releases():
-    return "List of all releases"
+@app.get("/releases/", response_model=List[ReleaseSchema])
+async def get_releases():
+    query = releases.select()
+    return await database.fetch_all(query)
 
 @app.get("/releases/{_id}")
 def get_release(_id: int):
     return f"Release with id {_id}"
 
-@app.post("/releases/")
-def create_release(data: dict):
-    return "Create a release using given data"
+@app.post("/releases/", response_model=ReleaseSchema)
+async def create_release(release: ReleaseSchemaCreateIn):
+    query = releases.insert().values(**release.dict())
+    last_record_id = await database.execute(query)
+    return {**release.dict(), "id": last_record_id}
 
 @app.put("/releases/{_id}")
 def update_release(_id: int, data: dict):
