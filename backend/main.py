4c1e63b91aadac3af51158b2eadcc47bdbbe6511
Papi Diagne <abdiagne96@gmail.com>
2022-07-18 15:09:35 +0000

    feat(backend): add Release pydantic model with data validation

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
+from datetime import datetime
+from typing import Optional, List, Dict
+from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Status(str, Enum):
+	planned= "planned"
+	ongoing= "ongoing"
+	done= "done"
+
+class ReleaseSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime = datetime.now()
+    status: Status = Status.planned
+    additional_info: Optional[str]
+    steps: List[Dict] = [{"label": "All relevant Github pull requests have been merged", "status": "off"},
+		{"label": "CHANGELOG.md files have been updated", "status": "off"},
+		{"label": "All tests are passing", "status": "off"},
+		{"label": "Releases in Github created", "status": "off"},
+		{"label": "Deployed in demo", "status": "off"},
+		{"label": "Tested thoroughly in demo", "status": "off"},
+		{"label": "Deployed in production", "status": "off"},
+	]
 
 @app.get("/")
 def home():
